f1ac501c7d19f9d85f0be5adcca390a844ebb664
Bryan Hoffmann <bryanhoffmanndev@gmail.com>
2022-12-22 13:36:12 -0300

    Project created

---
+++ src/main/java/com/bryanrho/apispringmongo/ApiSpringmongoApplication.java
@@ -0,0 +1,13 @@
+package com.bryanrho.apispringmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSpringmongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiSpringmongoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bryanrho/apispringmongo/ApiSpringmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bryanrho.apispringmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiSpringmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
