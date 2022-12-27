774ad6076d44bd7df5f7696c4f598bfe0a18241e
Bryan Hoffmann <bryanhoffmanndev@gmail.com>
2022-12-26 22:16:00 -0300

    Operação de instanciação da base de dados

---
+++ src/main/java/com/bryanrho/apispringmongo/config/Instantiation.java
@@ -0,0 +1,29 @@
+package com.bryanrho.apispringmongo.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import com.bryanrho.apispringmongo.domain.User;
+import com.bryanrho.apispringmongo.repository.UserRepository;
+
+@Configuration
+public class Instantiation implements CommandLineRunner {
+
+	@Autowired
+	private UserRepository userRepository;
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		userRepository.deleteAll();
+
+		User maria = new User(null, "Maria Brown", "maria@gmail.com");
+		User alex = new User(null, "Alex Green", "alex@gmail.com");
+		User bob = new User(null, "Bob Grey", "bob@gmail.com");
+
+		userRepository.saveAll(Arrays.asList(maria,alex,bob));
+	}
+}
