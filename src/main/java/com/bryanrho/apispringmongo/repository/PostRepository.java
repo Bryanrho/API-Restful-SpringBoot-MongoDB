fa06848bb22e60596a2abbf737249cb214bab773
Bryan Hoffmann <bryanhoffmanndev@gmail.com>
2022-12-28 18:10:41 -0300

    Consulta simples com @Query

--- src/main/java/com/bryanrho/apispringmongo/repository/PostRepository.java
+++ src/main/java/com/bryanrho/apispringmongo/repository/PostRepository.java
@@ -3,6 +3,7 @@ package com.bryanrho.apispringmongo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.bryanrho.apispringmongo.domain.Post;
@@ -10,5 +11,8 @@ import com.bryanrho.apispringmongo.domain.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
 
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+	List<Post> findByTitle(String text);
+	
 	List<Post> findByTitleContainingIgnoreCase(String text);
 }
